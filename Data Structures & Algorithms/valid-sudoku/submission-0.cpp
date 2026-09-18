fad877b2da7b22a2da42a14fd1c329aa4ef1bcf7
DagarX101101 <rmdagar2006@gmail.com>
2026-09-19 00:46:12 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        // 9 sets for rows
+        unordered_set<char> rows[9];
+
+        // 9 sets for columns
+        unordered_set<char> cols[9];
+
+        // 9 sets for 3x3 boxes
+        unordered_set<char> boxes[9];
+
+        for(int i = 0; i < 9; i++) {
+
+            for(int j = 0; j < 9; j++) {
+
+                // Ignore empty cells
+                if(board[i][j] == '.')
+                    continue;
+
+                char num = board[i][j];
+
+                // Find which 3x3 box this cell belongs to
+                int box = (i / 3) * 3 + (j / 3);
+
+                // Check row
+                if(rows[i].count(num))
+                    return false;
+
+                // Check column
+                if(cols[j].count(num))
+                    return false;
+
+                // Check 3x3 box
+                if(boxes[box].count(num))
+                    return false;
+
+                // Remember this number
+                rows[i].insert(num);
+                cols[j].insert(num);
+                boxes[box].insert(num);
+            }
+        }
+
+        return true;
+    }
+};
