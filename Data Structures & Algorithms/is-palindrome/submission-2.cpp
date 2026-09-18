b26ce60a5b4021b159e8edad2a9641cd7c63e7b0
DagarX101101 <rmdagar2006@gmail.com>
2026-09-19 01:32:50 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        string ans = "";
+
+        for(char c : s) {
+
+            if(isalnum(c)) {
+                c = tolower(c);
+                ans += c;
+            }
+        }
+
+        int i = 0;
+        int j = ans.length() - 1;
+
+        while(i <= j) {
+
+            if(ans[i] != ans[j])
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
